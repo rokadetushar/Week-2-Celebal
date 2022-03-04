d8478cf14deab2d79ffefdd794a085664c2871c2
Tushar Rokade <95062408+rokadetushar@users.noreply.github.com>
2022-03-04 10:32:14 +0530

    Add files via upload

---
+++ OPERATOR.py
@@ -0,0 +1,23 @@
+x = 8
+y = 12
+
+print('x + y =',x+y)
+
+print('x - y =',x-y)
+
+print('x * y =',x*y)
+
+print('x / y =',x/y)
+
+print('x // y =',x//y)
+
+print('x ** y =',x**y)
+
+
+# #output
+# x + y = 20
+# x - y = -4
+# x * y = 96
+# x / y = 0.6666666666666666
+# x // y = 0
+# x ** y = 68719476736
---
+++ comparision.py
@@ -0,0 +1,16 @@
+#  Python program to show
+# shift operators
+
+a = 50
+b = -50
+
+# print bitwise right shift operator
+print("a >> 1 =", a >> 1)
+print("b >> 1 =", b >> 1)
+
+a = 7
+b = -10
+
+# print bitwise left shift operator
+print("a << 1 =", a << 1)
+print("b << 1 =", b << 1)
